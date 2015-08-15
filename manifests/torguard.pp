a9623188dac6ee34eef9f0d25c161faaa1d96e51
ronen <narkisr@gmail.com>
2015-08-16 01:08:34 +0300

    adding pass file and binary exec

--- manifests/torguard.pp
+++ manifests/torguard.pp
@@ -1,11 +1,35 @@
 # Setting up Torguard
-class vpnize::torguard ($src = '/vagrant/TorGuardPRO.zip') {
+class vpnize::torguard (
+  $src = '/vagrant',
+  $archive = 'TorGuardPRO',
+  $username = 'foo',
+  $password = 'bar'
+) {
 
-  archive::extract {'torguard configuration':
-    src_target => $src
+  archive::extract {$archive:
+    src_target => $src,
+    target     => '/opt',
+    extension  => 'zip'
   }
 
   package{'openvpn':
     ensure  => present
   }
+
+  file { '/usr/bin/torguard':
+    ensure  => file,
+    content => "cd /opt/${archive} && openvpn --config \"TorGuard.\$1.ovpn\" --auth-user-pass pass.txt",
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
+  file { "/opt/${archive}":
+    ensure  =>  file,
+    mode    =>  '0644',
+    content =>  template('torguard-pass.erb'),
+    owner   =>  root,
+    group   =>  root,
+  }
+
 }
